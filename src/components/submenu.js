f99a8f3d0669a6f406ffa5c5cd31684ddc43c5b8
Guilherme Nunes <guilhermenunes09@protonmail.com>
2020-08-03 16:05:43 -0300

    Submenu Desktop done

--- src/components/submenu.js
+++ src/components/submenu.js
@@ -1,26 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import "./submenu.css";
 import { Link as LinkSame, scroller, Events } from 'react-scroll'
-import { useSpring, animated, config } from 'react-spring';
+import { useSpring, useTransition, animated, config } from 'react-spring';
+import { Handle } from 'rc-slider';
+import { withRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
-
-function SubMenu () {
-
-    const props = useSpring({
-        delay: 1000,
-        config: {
-            duration: 2000,
-            delay: 2000,
-        },
-        from: {
-            opacity: 0,
-        }, to: {
-            opacity: 1,
-        }
-    })
-
+function SubMenuDom (props) {
     return(
-        <animated.div style={props} className="submenu d-flex flex-row justify-content-center align-items-center rounded">
+        <>
             <LinkSame to="quemsomos" smooth={true}>
                 <div className="submenu-item">
                     Quem Somos
@@ -46,7 +33,96 @@ function SubMenu () {
                     Depoimentos
                 </div>
             </LinkSame>
-        </animated.div>
+        </>
+    );
+}
+
+
+function SubMenu () {
+
+    const someRefName = useRef();
+    const [sticky, setSticky] = useState(0);
+    let offset = 0;
+    const index=0;
+    
+    const propsSpring2 = useSpring({
+        config: { duration: 300 },
+    
+            opacity: sticky ? 1 : 0.9,
+            position: sticky ? 'fixed' : 'absolute',
+            color: sticky ? 'black' : 'white',
+      
+            borderBottom: '4px solid black',
+            backgroundColor: sticky ? 'white' : 'black',
+            top: sticky ? 0 : 'auto',
+            left: '50%',
+            fontSize: '1em',
+            height: '35px',
+            minWidth: sticky ? '100%' : '30%',
+            zIndex: 200,
+            bottom: '63px', 
+            maxWidth: '98%',
+            transform: 'translateX(-50%)',
+
+
+    })
+
+    const propsSpring = useSpring({
+        delay: 1000,
+        immediate: false,
+        config: {
+            duration: 2000,
+            delay: 2000,
+        },
+        from: {
+            opacity: 0,
+        }, to: {
+            opacity: 1,
+        }
+    });
+
+    function helloWorld () {
+        console.log("Hello World");
+    }
+
+
+    function handleOnScroll (scrollY, offset){
+        console.log("OFFSET");
+        console.log(offset)
+        if(scrollY >= offset) {
+            if(sticky === 0) {
+                setSticky(1);
+            }
+        } else {
+            setSticky(0);
+        }
+    }
+
+
+    useEffect(()=>{
+        const offsetTopDefine = someRefName.current.offsetTop;
+        offset = offsetTopDefine;
+    }, []);
+
+    useEffect(()=>{
+        window.addEventListener("scroll", () => handleOnScroll(window.scrollY, offset), {capture: true, passive: true});
+        return () => {
+            window.removeEventListener("scroll", handleOnScroll);
+        }
+    },[]);
+
+    const subMenuClass1 = "d-flex flex-row justify-content-center align-items-center rounded submenu";
+    const subMenuClass2 = "d-flex flex-row justify-content-center align-items-center rounded submenu-sticky";
+
+
+    return(
+            <>
+
+            <animated.div style={propsSpring2} className={subMenuClass1} ref={someRefName}>
+                <SubMenuDom/>
+            </animated.div>
+           
+        </>
     );
 }
 
